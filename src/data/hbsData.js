2b68b204179223aa05174114b0dee24523088005
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2023-03-07 20:28:04 +0200

    add

---
+++ src/data/hbsData.js
@@ -0,0 +1,18 @@
+export const base = {
+    name: "User",
+    position: "FE Developer",
+    language: "Javascript",
+  };
+  
+  export const list = {
+    frameworks: ["React", "Vue", "Angular"],
+    libs: ["moment", "lodash", "axios", "propTypes"],
+  };
+  
+  export const frameworks = ["React", "Vue", "Angular"];
+  
+  export const libs = [
+    { id: 1, lib: "React" },
+    { id: 2, lib: "Vue" },
+    { id: 3, lib: "Angular" },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import example from "./images/smoke.png";
-import imgSvg from "./images/flat.svg";
-import { sum } from "./helper/sum.js";
-import "./styles/main.scss";
+// import example from "./images/smoke.png";
+// import imgSvg from "./images/flat.svg";
+// import { sum } from "./helper/sum.js";
+// import "./styles/main.scss";
 
-console.log("Webpack");
-// // Create a class property without a constructor
-class Game {
-  name = "Violin Charades";
-}
-const myGame = new Game();
-// Create paragraph node
-const p = document.createElement("p");
-p.textContent = `I like ${myGame.name}.`;
+// console.log("Webpack");
+// // // Create a class property without a constructor
+// class Game {
+//   name = "Violin Charades";
+// }
+// const myGame = new Game();
+// // Create paragraph node
+// const p = document.createElement("p");
+// p.textContent = `I like ${myGame.name}.`;
 
-// Create heading node
-const heading = document.createElement("h1");
-heading.textContent = "Interesting!";
+// // Create heading node
+// const heading = document.createElement("h1");
+// heading.textContent = "Interesting!";
 
-// Append SVG and heading nodes to the DOM
-const app = document.querySelector("#root");
-app.append(heading, p);
+// // Append SVG and heading nodes to the DOM
+// const app = document.querySelector("#root");
+// app.append(heading, p);
 
-const img = document.createElement("img");
-img.src = example;
-app.append(img);
+// const img = document.createElement("img");
+// img.src = example;
+// app.append(img);
 
-const svgImg = document.createElement("img");
-svgImg.src = imgSvg;
-app.append(svgImg);
+// const svgImg = document.createElement("img");
+// svgImg.src = imgSvg;
+// app.append(svgImg);
 
-console.log(sum(2, 3));
+// console.log(sum(2, 3));
+
+
+import './styles/style.css'
